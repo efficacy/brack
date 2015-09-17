4b193514af04fed7bdd652df9dc6c4374bd686ab
Frank Carver <frank@efsol.com>
2015-09-17 23:45:28 +0100

    lexer pretty much done

--- lisp.js
+++ lisp.js
@@ -1,19 +1,3 @@
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('readable', function() {
-  var chunk = process.stdin.read();
-  if (chunk !== null) {
-    process.stdout.write('data: ' + chunk);
-    lex(chunk, null);
-  }
-});
-
-process.stdin.on('end', function() {
-  process.stdout.write('end');
-});
-
-var lex_buf = '';
-var lex_status = 'outside';
 
 function category(c) {
   if (c == '(') return 'open';
@@ -24,12 +8,66 @@ function category(c) {
   return 'letter';
 }
 
+var lex_buf = '';
+var lex_status = 'outside';
+var context = [];
+
+function parser(token) {
+  console.log('got token type: ' + token.type + ' value: ' + token.value);
+}
+
 function lex(s, parser) {
+  function emit(type) {
+    lex_status = 'outside';
+    var value = '' + lex_buf;
+    lex_buf = '';
+    parser({type: type, value: value});
+  }
   var n = s.length;
   for (var i = 0; i < n; ++i) {
     c = s[i];
     var cc = category(c);
-    console.log('c=' + c + ' category=' + cc);
+    console.log('status=' + lex_status + ' c=' + c + ' category=' + cc + ' buf=[' + lex_buf + ']');
+    switch(cc) {
+    case 'ws':
+      if (lex_status != 'outside') lex_buf += c;
+      if (lex_status == 'symbol') emit('symbol');
+      break;
+    case 'letter':
+      lex_buf += c;
+      if (lex_status == 'outside') lex_status = 'symbol';
+      break;
+    case 'sq':
+    case 'dq':
+      lex_buf += c;
+      if (lex_status == 'outside') {
+        lex_status = cc;
+      } else if (lex_status == cc) {
+      	emit('symbol');
+      } 
+      break;
+    case 'open':
+    case 'close':
+      if (lex_status == 'symbol') emit('symbol');
+      if (lex_status == 'sq' || lex_status == 'dq') {
+      	lex_buf += c;
+      } else {
+        emit(cc);
+      }
+      break;
+    }
   }
 }
 
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', function() {
+  var chunk = process.stdin.read();
+  if (chunk !== null) {
+    lex(chunk, parser);
+  }
+});
+
+process.stdin.on('end', function() {
+  process.stdout.write('end');
+});
